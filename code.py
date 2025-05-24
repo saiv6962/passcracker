e1a7c91303686a35a5b7bf0e87baa9a059ed4b5e
sai <122634863+saiv6962@users.noreply.github.com>
2025-05-24 11:47:43 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+import hashlib
+
+
+def crackHash(inputPass):
+    try:
+        passFile = open("wordlist.txt", "r")
+    except:
+        print("Could not find file.")
+
+    for password in passFile:
+        encPass = password.encode("utf-8")
+        digest = hashlib.md5(encPass.strip()).hexdigest()
+        if digest == inputPass:
+            print("Password Found: " + password)
+
+if __name__ == '__main__':
+    crackHash("")
